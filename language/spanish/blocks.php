cfcff1d58a41cb24e8c490dae94d62d27fb620b7
Juan Calzado <debianus@impresscms.org>
2008-02-13 00:06:30 +0000

    Updating and improving the Smartsection Spanish translation files
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/smartsection/trunk@877 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/spanish/blocks.php
+++ language/spanish/blocks.php
@@ -1,4 +1,4 @@
-<?php
+<?php // Traducción de www.riosoft.es | www.rioxoops.es
 
 /**
 * $Id$
@@ -26,31 +26,42 @@ include_once(XOOPS_ROOT_PATH . "/modules/smartsection/language/" . $xoopsConfig[
 */
 // Blocks
 
-define("_MB_SSECTION_ALLCAT", "Todas las categor&iacute;as");
-define("_MB_SSECTION_AUTO_LAST_ITEMS", "&iquest;Desplegar autom&aacute;ticamente el &uacute;ltimo elemento(s)?");
-define("_MB_SSECTION_CATEGORY", "Categor&iacute;a");
-define("_MB_SSECTION_CHARS", "Longitud del titulo");
+define("_MB_SSECTION_ALLCAT", "Todas las categorías");
+define("_MB_SSECTION_AUTO_LAST_ITEMS", "¿Mostrar automáticamente las últimas publicaciones?");
+define("_MB_SSECTION_CATEGORY", "Categoría");
+define("_MB_SSECTION_CHARS", "Longitud el título");
 define("_MB_SSECTION_COMMENTS", "Comentario(s)");
-define("_MB_SSECTION_DATE", "Fecha de publicaci&oacute;n");
-define("_MB_SSECTION_DISP", "Desplegar");
-define("_MB_SSECTION_DISPLAY_CATEGORY", "&iquest;Desplegar el nombre de la categor&iacute;a?");
-define("_MB_SSECTION_DISPLAY_COMMENTS", "&iquest;Desplegar la cuenta de comentarios?");
-define("_MB_SSECTION_DISPLAY_TYPE", "Tipo de despliege:");
+define("_MB_SSECTION_DATE", "Fecha de Publicación");
+define("_MB_SSECTION_DISP", "Visualizar");
+define("_MB_SSECTION_DISPLAY_CATEGORY", "¿Mostrar el nombre de la categoría?");
+define("_MB_SSECTION_DISPLAY_COMMENTS", "¿Mostrar el número de comentarios?");
+define("_MB_SSECTION_DISPLAY_TYPE", "Mostrar tipo :");
 define("_MB_SSECTION_DISPLAY_TYPE_BLOCK", "Cada elemento es un bloque");
 define("_MB_SSECTION_DISPLAY_TYPE_BULLET", "Cada elemento es un punto");
-define("_MB_SSECTION_DISPLAY_WHO_AND_WHEN", "&iquest;Desplegar quien env&iacute;a y la fecha?");
-define("_MB_SSECTION_FULLITEM", "Leer el art&iacute;culo completo");
-define("_MB_SSECTION_HITS", "N&uacute;mero de hits");
-define("_MB_SSECTION_ITEMS", "Art&iacute;culos");
-define("_MB_SSECTION_LAST_ITEMS_COUNT", "S&iacute; si, &iquest;cu&aacute;ntos art&iacute;culos a desplegar?");
+define("_MB_SSECTION_DISPLAY_WHO_AND_WHEN", "¿Mostrar autor y fecha?");
+define("_MB_SSECTION_FULLITEM", "Leer el artículo completo");
+define("_MB_SSECTION_HITS", "Lecturas");
+define("_MB_SSECTION_ITEMS", "Artículos");
+define("_MB_SSECTION_LAST_ITEMS_COUNT", "si dijo sí, ¿cuántos elementos han de mostrarse?");
 define("_MB_SSECTION_LENGTH", " caracteres");
-define("_MB_SSECTION_ORDER", "Orden de desplegado");
+define("_MB_SSECTION_ORDER", "Orden");
 define("_MB_SSECTION_POSTEDBY", "Publicado por");
-define("_MB_SSECTION_READMORE", "Leer m&aacute;s...");
+define("_MB_SSECTION_READMORE", "Leer más...");
 define("_MB_SSECTION_READS", "lecturas");
-define("_MB_SSECTION_SELECT_ITEMS", "S&iacute; no, seleccione los art&iacute;culos a desplegar:");
-define("_MB_SSECTION_SELECTCAT", "Desplegar los art&iacute;culos de:");
-define("_MB_SSECTION_VISITITEM", "Visitar el");
-define("_MB_SSECTION_WEIGHT", "Lista por peso");
+define("_MB_SSECTION_SELECT_ITEMS", "si dijo no, determine qué artículos han de ser mostrados :");
+define("_MB_SSECTION_SELECTCAT", "Mostrar artículos de :");
+define("_MB_SSECTION_VISITITEM", "Visitar");
+define("_MB_SSECTION_WEIGHT", "Lista por 'peso'");
 define("_MB_SSECTION_WHO_WHEN", "Publicado por %s el %s");
+//bd tree block hack
+define("_MB_SSECTION_LEVELS", "niveles");
+define("_MB_SSECTION_CURRENTCATEGORY", "Categoría actual");
+define("_MB_SSECTION_ASC", "ASC");
+define("_MB_SSECTION_DESC", "DESC");
+define("_MB_SSECTION_SHOWITEMS", "Mostrar Elementos");
+//--/bd
+
+define("_MB_SSECTION_FILES", "archivos");
+define("_MB_SSECTION_DIRECTDOWNLOAD", "¿Enlace directo al archivo en lugar de al artículo?");
+
 ?>
--- language/spanish/common.php
+++ language/spanish/common.php
@@ -1,4 +1,4 @@
-<?php
+<?php // Traducción de www.riosoft.es | www.rioxoops.es
 
 /**
 * $Id$
@@ -7,7 +7,15 @@
 * Licence: GNU
 */
 
-define("_SMARTSECTION_MESSAGE_FILE_ERROR", "Error: Incapaz de guardar el archivo cargado por la siguiente raz&oacute;n:<br />%s");
-define("_SMARTSECTION_MESSAGE_WRONG_MIMETYPE", "Error: Tipo de archivo no permitido. Favor de reenv&iacute;ar.");
+define("_SMARTSECTION_MESSAGE_FILE_ERROR", "Error: fue imposible almacenar el documento subido por las siguientes razones:<br />%s");
+define("_SMARTSECTION_MESSAGE_WRONG_MIMETYPE", "Error: el tipo de archivo no está permitido. Por favor, vuelva a subirlo.");
+
+define("_AM_SSECTION_OPTIONS", "Opciones");
+define("_AM_SSECTION_DISPLAY_SUMMARY", "¿Mostrar sumario en la página del artículo?");
+define("_AM_SSECTION_DOHTML", " Permitir etiquetas HTML");
+define("_AM_SSECTION_DOIMAGE", " Permitir imágenes");
+define("_AM_SSECTION_DOLINEBREAK", " Permitir 'linebreak'");
+define("_AM_SSECTION_DOSMILEY", " Permitir caritas");
+define("_AM_SSECTION_DOXCODE", " Permitir ImpressCMS codes");
 
 ?>
