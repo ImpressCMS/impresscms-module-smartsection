03b7c587e0a8533693baf67ccd2b0ccefc9d593d
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-06-03 22:25:36 +0000

    modules - SmartSection - (portuguesebr) part 4 - Converted as UTF-8 - Translation finalized...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/smartsection/trunk@2641 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/smartdbupdater.php
+++ language/portuguesebr/smartdbupdater.php
@@ -7,12 +7,12 @@
 * Licence: GNU
 */
 if (!defined("XOOPS_ROOT_PATH")) {
- 	die("O caminho raiz de seu Site n„o est· definido");
+ 	die("O caminho raiz de seu Site n√£o est√° definido");
 }
 
 define("_SDU_IMPORT", "Importar");
-define("_SDU_CURRENTVER", "Vers„o Atual: <span class='currentVer'>%s</span>");
-define("_SDU_DBVER", "Vers„o do Banco de Dados %s");
+define("_SDU_CURRENTVER", "Vers√£o Atual: <span class='currentVer'>%s</span>");
+define("_SDU_DBVER", "Vers√£o do Banco de Dados %s");
 define("_SDU_MSG_ADD_DATA", "Os dados adicionados na tabela %s");
 define("_SDU_MSG_ADD_DATA_ERR", "Erro - enquanto adicionava dados na tabela %s");
 define("_SDU_MSG_CHGFIELD", "Alterando campo %s na tabela %s");
@@ -21,13 +21,13 @@ define("_SDU_MSG_CREATE_TABLE", "Tabela %s criada");
 define("_SDU_MSG_CREATE_TABLE_ERR", "Erro - criando tabela %s");
 define("_SDU_MSG_NEWFIELD", "Campos adicionados com sucesso %s");
 define("_SDU_MSG_NEWFIELD_ERR", "Erro - enquando adicionava campo %s");
-define("_SDU_NEEDUPDATE", "Seu banco de dado est· desatualizado. Atualizar o seu banco de dados e tabelas!<br><b>Note : O SmartFactory recomenda que vocÍ faÁa um backup de todas as tabelas do SmartSection antes de rodar este script de atualizaÁ„o. Caso n„o faÁa isto, v· por sua conta e risco e boa sorte.</b><br>");
-define("_SDU_NOUPDATE", "Seu banco de dados est· atualizado. N„o È necess·rio fazer atualizaÁıes.");
+define("_SDU_NEEDUPDATE", "Seu banco de dado est√° desatualizado. Atualizar o seu banco de dados e tabelas!<br><b>Note : O SmartFactory recomenda que voc√™ fa√ßa um backup de todas as tabelas do SmartSection antes de rodar este script de atualiza√ß√£o. Caso n√£o fa√ßa isto, v√° por sua conta e risco e boa sorte.</b><br>");
+define("_SDU_NOUPDATE", "Seu banco de dados est√° atualizado. N√£o √© necess√°rio fazer atualiza√ß√µes.");
 define("_SDU_UPDATE_DB", "Atualizando seu banco de dados");
-define("_SDU_UPDATE_ERR", "Erro - Enquando atualizava a vers„o %s");
+define("_SDU_UPDATE_ERR", "Erro - Enquando atualizava a vers√£o %s");
 define("_SDU_UPDATE_NOW", "Atualizar Agora!");
-define("_SDU_UPDATE_OK", "AtualizaÁ„o foi concluida com Sucesso para a vers„o %s");
-define("_SDU_UPDATE_TO", "Atualizando a vers„o %s");
+define("_SDU_UPDATE_OK", "Atualiza√ß√£o foi concluida com Sucesso para a vers√£o %s");
+define("_SDU_UPDATE_TO", "Atualizando a vers√£o %s");
 define("_SDU_UPDATE_UPDATING_DATABASE", "Atualizando o banco de dados...");
 
 
