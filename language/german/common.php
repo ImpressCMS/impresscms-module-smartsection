12da99a46aa0e9c8a6b98aaddcac738ebc93db1f
xoops-magazine <xoops-magazine@tld.com>
2008-06-23 05:48:38 +0000

    smartsection German UTF-8 Part2
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/smartsection/trunk@3171 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/german/common.php
+++ language/german/common.php
@@ -7,14 +7,14 @@
 * Licence: GNU
 */
 
-define("_SMARTSECTION_MESSAGE_FILE_ERROR", "Fehler: Aus den nachstehenden Gr�nden war es nicht m�glich, die hochgeladene Datei zu sichern:<br />%s");
+define("_SMARTSECTION_MESSAGE_FILE_ERROR", "Fehler: Aus den nachstehenden Gründen war es nicht möglich, die hochgeladene Datei zu sichern:<br />%s");
 define("_SMARTSECTION_MESSAGE_WRONG_MIMETYPE", "Fehler: Dateiformat ist nicht erlaubt, bitte wiederholen Sie Ihre Einsendung.");
 
 define("_AM_SSECTION_OPTIONS", "Optionen");
 define("_AM_SSECTION_DISPLAY_SUMMARY", "Zusammenfassung auf Artikelseite anzeigen?");
 define("_AM_SSECTION_DOHTML", " Erlaube HTML Tags");
 define("_AM_SSECTION_DOIMAGE", " Erlaube Bilder");
-define("_AM_SSECTION_DOLINEBREAK", " Erlaube Zeilenumbr�che");
+define("_AM_SSECTION_DOLINEBREAK", " Erlaube Zeilenumbrüche");
 define("_AM_SSECTION_DOSMILEY", " Erlaube Smiley Icons");
 define("_AM_SSECTION_DOXCODE", " Erlaube ImpressCMS Codes");
 ?>
