cb917636ccc67a126df883208843284a7e3480d6
Vaughan Montgomery <vaughan@impresscms.org>
2008-09-27 15:25:28 +0000

    changed references to class/xoopsmodule.php, now refers to kernel/module.php
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/smartsection/trunk@5183 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -13,7 +13,7 @@ if (!defined("SMARTSECTION_NOCPFUNC")) {
 	include_once '../../../include/cp_header.php';
 }
 
-include_once XOOPS_ROOT_PATH . "/class/xoopsmodule.php";
+require_once XOOPS_ROOT_PATH.'/kernel/module.php';
 include_once XOOPS_ROOT_PATH . "/class/xoopstree.php";
 include_once XOOPS_ROOT_PATH . "/class/xoopslists.php";
 include_once XOOPS_ROOT_PATH . '/class/pagenav.php';
